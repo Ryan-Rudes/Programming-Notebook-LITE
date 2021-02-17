6ad504caeb170f5c3f942ac111489de761086094
Ryan Rudes <ryanrudes@gmail.com>
2021-02-17 13:19:33 -0500

    Initial Commit

---
+++ Programming Notebook LITE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Programming Notebook LITE
+//
+//  Created by Ryan Rudes on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Programming Notebook LITE/Programming_Notebook_LITEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_Notebook_LITEApp.swift
+//  Programming Notebook LITE
+//
+//  Created by Ryan Rudes on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_Notebook_LITEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
